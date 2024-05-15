334adf8b8954240477a6bb08a519d26e9f67cd13
Jermaine Forbes <jermaine0forbes@gmail.com>
2024-05-14 22:24:05 -0400

    creating a settings page pt2

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,7 @@
 import React, {useContext, useState, useEffect, Fragment}  from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import { TextField, InputAdornment} from '@material-ui/core';
+import { TextField, InputAdornment, Grid, FormControl, FormGroup} from '@material-ui/core';
 import { useNavigate } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
 import {AuthContext} from '../providers/AuthProvider';
@@ -31,21 +31,28 @@ export default function Home()
                 <main>
                     <Typography component="h3">Settings</Typography>
 
-                    <Box component="section">
-                        <TextField
-                            label="Number of Pokemon"
-                            id="outlined-start-adornment"
-                            sx={{ m: 1, width: '25ch' }}
-                            type="number"
-                        />
-                        <TextField
-                        label="Time to memorize"
-                        id="outlined-start-adornment-2"
-                        sx={{ m: 1, width: '25ch' }}
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start">seconds</InputAdornment>,
-                        }}
-                    />
+                    <Box component="form">
+                        <Grid container style={{display: "flow"}}>
+                            {/* <FormControl> */}
+                                <TextField
+                                    label="Number of Pokemon"
+                                    id="outlined-start-adornment"
+                                    sx={{ m: 1, width: '25ch' }}
+                                    type="number"
+                                />
+                            {/* </FormControl> */}
+                            {/* <FormControl> */}
+                                <TextField
+                                    label="Time to memorize"
+                                    id="outlined-start-adornment-2"
+                                    sx={{ m: 1, width: '25ch' }}
+                                    InputProps={{
+                                        startAdornment: <InputAdornment position="start">seconds</InputAdornment>,
+                                    }}
+                                />
+                            {/* </FormControl> */}
+                        </Grid>
+
                     </Box>
                 </main>
             </Box>
