7428c8103843259baf0336d3aa353842ea4ec981
Jermaine Forbes <jermaine0forbes@gmail.com>
2025-01-30 17:33:56 -0500

    styling entry component pt2

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -19,17 +19,51 @@ const useStyles = makeStyles(() => ({
     entry: {
         marginRight:'15px',
         flewGrow: 0,
-        color:'white',
+       
         fontSize:'1em',
         textTransform:'capitalize',
     },
     entryBlock: {
         display: 'flex',
         alignItems: 'center',
-        backgroundColor: '#4d4d4d',
+        padding: "1em",
+       
         '&> p': {
-            textTransform:'uppercase'
+            textTransform:'uppercase',
+            fontSize: "0.8em"
+        },
+
+        "& .MuiTextField-root":{
+            width:"100%",
+            marginLeft: "1em",
         }
+
+    },
+    entryHead: {
+        fontSize:"1.2em",
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        borderRadius: "10px 10px 0 0",
+        padding: "1em", 
+        marginBottom:"2px",
+        
+    },
+    entrySection: {
+       
+        color:'white',
+
+        "& .MuiFilledInput-root": {
+            backgroundColor:"transparent",
+            
+        },
+        "& .MuiFilledInput-underline:before": {
+            borderColor: "white"
+        }
+    }, 
+    entryBody: {
+        backgroundColor: '#4d4d4d',
+        borderRadius: "0 0 10px 10px", 
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        marginBottom: "1em"
     }
 }));
 
@@ -40,11 +74,11 @@ export default function Entry({num = 0})
     const classes = useStyles();
     
     return (
-        <Box component={'section'}>
-            <Grid>
+        <Box component={'section'} className={classes.entrySection}>
+            <Grid className={classes.entryHead}>
                 #{num}
             </Grid>
-            <Grid>
+            <Grid className={classes.entryBody}>
                 <Grid className={classes.entryBlock}>
                     <Typography>Title:</Typography>
                     
@@ -56,13 +90,24 @@ export default function Entry({num = 0})
                     ></TextField>
                 </Grid>
                 <Grid className={classes.entryBlock}>
-                <Typography>Entry:</Typography>
+                    <Typography>Entry:</Typography>
                     <TextField
-                        label="Multiline"
+                        label="Enter the answer"
                         multiline
                         variant='filled'
                         className={classes.entry}
-                        rows={4}
+                        rows={3}
+                    >
+                    </TextField>
+                </Grid>
+                <Grid className={classes.entryBlock}>
+                    <Typography>Tags:</Typography>
+                    <TextField
+                        label="Enter any tags"
+                        multiline
+                        variant='filled'
+                        className={classes.entry}
+                        rows={1}
                     >
                     </TextField>
                 </Grid>
